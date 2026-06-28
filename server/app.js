7b065ed9185f457971fcd4f93a285a2ebd40d520
Balavant Rajendra Kulkarni <BalavantK@emids.com>
2026-06-28 19:24:29 +0530

    change

--- server/app.js
+++ server/app.js
@@ -59,7 +59,7 @@ app.post('/api/convert', async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 5000
+const PORT = 8080
 
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`)
